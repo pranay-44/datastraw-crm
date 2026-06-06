0d165c9155fe144bd9ee17f2657a06ba9c9d00cc
Pranay Bhalerao <pranaybhalerao44@gmail.com>
2026-06-06 11:44:02 +0530

    Update app.py for production readiness

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ with app.app_context():
     print("Database creation attempt finished!")
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
